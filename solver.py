55a5008a17d84b11d08a03ff7f4a2eee4e1cae2f
mishanya228 <olomishcak@outlook.com>
2019-11-08 00:34:15 +0300

    total error plot partially fixed

--- solver.py
+++ solver.py
@@ -29,14 +29,13 @@ class ConstantException(SolverException):
 
 
 class Solver:
-    _max_local_error = NINF
 
     def validate(self):
         if self.y0 == 0.0:
             raise YAxisDomainException("Given value of y does not belong to the domain!")
         if self.X - self.x0 <= 0:
             raise IntervalException(f"Given [{self.x0} ... {self.X}] interval doesn't exist!")
-        if self.N <= 1:
+        if self.N <= 0:
             raise NumberOfStepsException("Given number of steps is invalid!")
         if isnan(self.C) or isinf(self.C):
             raise ConstantException("Value of constant is either too big or can't be calculated!")
@@ -79,15 +78,15 @@ class Solver:
             self.method_name = "Runge-Kutta"
             self.method = self._solve_runge_kutta
 
-        self.exact_solution = Dataset(self.N).add_name("Exact Solution")
-        self.numerical_solution = Dataset(self.N).add_name(f"Numerical Solution ({self.method_name})")
-        self.local_error = Dataset(self.N).add_name(f"Local Error ({self.method_name})")
+        self.exact_solution = Dataset(self.N + 1).add_name("Exact Solution")
+        self.numerical_solution = Dataset(self.N + 1).add_name(f"Numerical Solution ({self.method_name})")
+        self.local_error = Dataset(self.N + 1).add_name(f"Local Error ({self.method_name})")
         self.total_error = Dataset(self.N).add_name(f"Total Error ({self.method_name})")
 
     def solve_exact(self):
         x = self.x0
         s = self.step
-        for i in range(self.N):
+        for i in range(self.N + 1):
             try:
                 self.exact_solution.insert(i, (x, self.y_exact(x)))
             except:
@@ -97,7 +96,6 @@ class Solver:
 
     def solve_numerical(self):
         self.method()
-        self._max_local_error = NINF
         self.get_total_error()
         return self.numerical_solution, self.local_error, self.total_error
 
@@ -105,20 +103,22 @@ class Solver:
         return y + h*self.y_prime(x, y)
 
     def _solve_euler(self):
+        _max_local_error = -1
         x = self.x0
         y = self.y0
         N = self.N
         h = self.step
         self.numerical_solution.insert(0, (x, y))  # x0 = x0, y0 = y0
         self.local_error.insert(0, (x, 0))
-        for i in range(1, N):
+        for i in range(1, N + 1):
             y = self._next_euler(x, y, h)     # next y
             x += h                            # next x
             y_exact = self.y_exact(x)
             error = y_exact - self._next_euler(x, y_exact, h)
-            self._max_local_error = abs(error) if abs(error) > self._max_local_error else self._max_local_error
+            _max_local_error = abs(error) if abs(error) > _max_local_error else _max_local_error
             self.numerical_solution.insert(i, (x, y))
             self.local_error.insert(i, (x, error))
+        return _max_local_error
 
     def _next_improved_euler(self, x, y, h):
         k1 = self.y_prime(x, y)
@@ -126,20 +126,22 @@ class Solver:
         return y + (h / 2) * (k1 + k2)
 
     def _solve_improved_euler(self):
+        _max_local_error = -1
         x = self.x0
         y = self.y0
         N = self.N
         h = self.step
         self.numerical_solution.insert(0, (x, y))  # x0 = x0, y0 = y0
         self.local_error.insert(0, (x, 0))
-        for i in range(1, N):
+        for i in range(1, N + 1):
             y = self._next_improved_euler(x, y, h)  # next y
             x += h  # next x
             y_exact = self.y_exact(x)
             error = y_exact - self._next_improved_euler(x, y_exact, h)
-            self._max_local_error = abs(error) if abs(error) > self._max_local_error else self._max_local_error
+            _max_local_error = abs(error) if abs(error) > _max_local_error else _max_local_error
             self.numerical_solution.insert(i, (x, y))
             self.local_error.insert(i, (x, error))
+        return _max_local_error
 
     def _next_runge_kutta(self, x, y, h):
         k1 = self.y_prime(x, y)
@@ -149,30 +151,30 @@ class Solver:
         return y + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
 
     def _solve_runge_kutta(self):
+        _max_local_error = -1
         x = self.x0
         y = self.y0
         N = self.N
         h = self.step
         self.numerical_solution.insert(0, (x, y))  # x0 = x0, y0 = y0
         self.local_error.insert(0, (x, 0))
-        for i in range(1, N):
+        for i in range(1, N + 1):
             y = self._next_runge_kutta(x, y, h)  # next y
             x += h  # next x
             y_exact = self.y_exact(x)
             error = y_exact - self._next_runge_kutta(x, y_exact, h)
-            self._max_local_error = abs(error) if abs(error) > self._max_local_error else self._max_local_error
+            _max_local_error = abs(error) if abs(error) > _max_local_error else _max_local_error
             self.numerical_solution.insert(i, (x, y))
             self.local_error.insert(i, (x, error))
+        return _max_local_error
 
     def get_total_error(self):
         # TODO: fix bug with the curve
         N = self.N
-        for i in range(2, N + 1):
+        for i in range(1, N + 1):
             self.N = i
-            self.method()
-            error = self._max_local_error
-            self.total_error.insert(i - 2, (i, error))
-            self._max_local_error = NINF
+            error = self.method()
+            self.total_error.insert(i - 1, (i, error))
         self.N = N
 
     def __str__(self):
